7a9754cba58844eb8d3062ac4854c9b66efb626d
Vladimir Khabarov <vkhabarov@alfabank.ru>
2022-08-15 22:51:19 +0300

    Initial commit
    • Create empty SwiftUI project
    • Set minimum iOS version to 15.0

---
+++ MoneyDayLimit/ContentView.swift
@@ -0,0 +1,16 @@
+//  Created by vs-khabarov on 12.08.2022.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoneyDayLimit/MoneyDayLimitApp.swift
@@ -0,0 +1,12 @@
+//  Created by vs-khabarov on 12.08.2022.
+
+import SwiftUI
+
+@main
+struct MoneyDayLimitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
